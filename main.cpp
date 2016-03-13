c7095798a4e750b4f049268261b7dd090c6a16d6
Adam P. Goucher <apgoucher@gmx.com>
2016-03-13 19:20:07 +0000

    Increased version number.

--- main.cpp
+++ main.cpp
@@ -23,7 +23,7 @@
 #include "includes/vlife.h"
 #include "includes/incubator.h"
 
-#define APG_VERSION "v3.05"
+#define APG_VERSION "v3.06"
 
 /*
  * Produce a new seed based on the original seed, current time and PID:
