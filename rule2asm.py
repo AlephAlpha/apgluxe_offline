79d57914ce9113abeabe2e1499249ba691b403c5
Adam P. Goucher <apgoucher@gmx.com>
2016-03-28 22:33:46 +0100

    Included remaining symmetries in hashsoup.h

--- rule2asm.py
+++ rule2asm.py
@@ -509,6 +509,10 @@ def main():
         cellrows = 40
     elif (symmetry == "D4_+1"):
         cellrows = 40
+    elif (symmetry == "D4_x4"):
+        cellrows = 40
+    elif (symmetry == "D4_x1"):
+        cellrows = 40
     elif (symmetry == "D8_4"):
         cellrows = 40
     elif (symmetry == "D8_1"):
@@ -516,7 +520,7 @@ def main():
     else:
         print("Invalid symmetry: \033[1;31m"+symmetry+"\033[0m is not one of the supported symmetries:")
         print("    C1, C2_4, C2_2, C2_1, C4_4, C4_1,")
-        print("    D2_+2, D2_+1, D2_x, D4_+4, D4_+2, D4_+1, D8_4, D8_1")
+        print("    D2_+2, D2_+1, D2_x, D4_+4, D4_+2, D4_+1, D4_x4, D4_x1, D8_4, D8_1")
         exit(1)
 
     print("Valid symmetry: \033[1;32m"+symmetry+"\033[0m")
