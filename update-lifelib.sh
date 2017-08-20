dc3cc72803deeb5301edeadf6158169162d64a44
Adam P. Goucher <goucher@statslab.cam.ac.uk>
2017-08-20 14:22:56 +0100

    No errors

--- update-lifelib.sh
+++ update-lifelib.sh
@@ -3,11 +3,11 @@
 if [ -d "lifelib/avxlife" ]
 then
 printf "\033[33;1mEnsuring lifelib is up-to-date...\033[0m\n"
+rm -rf "lifelib/avxlife/lifelogic" | true
+python lifelib/avxlife/rule2asm.py "b3s23" > /dev/null
 else
 printf "\033[33;1mDownloading lifelib...\033[0m\n"
 fi
 
-rm -rf "lifelib/avxlife/lifelogic" | true
-python lifelib/avxlife/rule2asm.py "b3s23" > /dev/null
 git submodule update --init
 
